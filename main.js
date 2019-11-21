b036ee9cbb5e3d513d04693fa61e1b82a3ad437f
gortron <gordon.lanza@gmail.com>
2019-11-21 09:02:02 +0000

    Done.

--- main.js
+++ main.js
@@ -1,20 +1,48 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = "♡";
+const FULL_HEART = "♥";
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  const errModal = document.querySelector("#modal");
+  errModal.className = "hidden";
 
+  const likeButtons = document.querySelectorAll(".like-glyph");
+  likeButtons.forEach(btn =>
+    btn.addEventListener("click", function() {
+      mimicServerCall("foo bar")
+        .then(clickedLikeButton(btn))
+        .catch(err => displayError(err));
+    })
+  );
 
+  function displayError(err) {
+    errModal.className = "";
+    errModal.innerText = err;
+    setTimeout(function() {
+      errModal.className = "hidden";
+    }, 3000);
+  }
 
+  function clickedLikeButton(btn) {
+    if (btn.className === "like-glyph activated-heart") {
+      btn.className = "like-glyph";
+      btn.innerText = EMPTY_HEART;
+    } else {
+      btn.className = "like-glyph activated-heart";
+      btn.innerText = FULL_HEART;
+    }
+  }
+});
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
+      let isRandomFailure = Math.random() < 0.2;
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
