1f04f8389aaba31ac02259bbed2f7d5cd23e8fb8
Jeremy Amram <jeremy.j.amram@gmail.com>
2019-11-25 15:09:26 +0000

    Done.

--- main.js
+++ main.js
@@ -4,7 +4,39 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const errorMessage = document.getElementById("modal");
+errorMessage.classList.add("hidden");
 
+const likeButtons = document.querySelectorAll(".like");
+
+likeButtons.forEach(function(likeButton){
+
+  likeButton.addEventListener("click", function(){
+    if(likeButton.firstElementChild.innerText === FULL_HEART) {
+      likeButton.classList.remove("activated-heart");
+      likeButton.firstElementChild.innerText = EMPTY_HEART;
+
+      mimicServerCall().then(function(result){
+        console.log(result)
+      })
+      
+    } else {
+      likeButton.firstElementChild.innerText = FULL_HEART;
+      likeButton.classList.add("activated-heart");
+
+      mimicServerCall().then(function(result){
+        console.log(result)
+      }).catch((error) => {
+        errorMessage.classList.remove("hidden");
+        console.log(error)
+        errorMessage.innerText = error;
+        setTimeout(function() {
+        errorMessage.classList.add("hidden");}, 5000);
+
+    })
+    }
+  })
+})
 
 
 //------------------------------------------------------------------------------
